8089023c87dbb45989d35124ad622e2d2b740403
underflow101 <ikarus125@gmail.com>
2020-03-06 14:30:05 +0900

    update
    Signed-off-by: underflow101 <ikarus125@gmail.com>

---
+++ imageResizer/imageResizer.py
@@ -0,0 +1,18 @@
+# resize image automation
+
+import glob, os
+from PIL import Image
+
+src = "/home/bearpaek/data/datasets/lpl/others/"
+dst = "/home/bearpaek/data/datasets/lpl224/others/"
+
+im_path = glob.glob(os.path.join(src, '*.jpg'))
+
+i = 0
+
+for filename in im_path:
+    print(filename)
+    img = Image.open(filename)
+    im = img.resize((224, 224), Image.ANTIALIAS)
+    im.save(dst + str(i) + '.jpg')
+    i += 1
