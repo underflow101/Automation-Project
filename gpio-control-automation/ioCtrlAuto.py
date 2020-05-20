51d9e36c5660b25191a10e3d1863a750f2d40583
underflow101 <ikarus125@gmail.com>
2020-05-20 09:51:23 +0900

    feat: Add Pointing Feature
    - Now user can point out a number s/he wants by typing integer
      between 0 ~ 127 to generate the function
    - integer less than 0 or greater than 127 will be ignored
    - char other than 'q' will be ignored
    
    Signed-off-by: underflow101 <ikarus125@gmail.com>

--- gpio-control-automation/ioCtrlAuto.py
+++ gpio-control-automation/ioCtrlAuto.py
@@ -16,7 +16,25 @@ for i in range(128):
     
     while 1:
         key = input().strip()
+
         if key == 'q':
             break
         else:
-            continue
+            try:
+                intKey = int(key)
+                if 0 <= intKey < 128:
+                    print("echo ", end='')
+                    print(key, end='')
+                    print(" > /sys/class/gpio/export")
+                    
+                    print("echo \"high\" > /sys/class/gpio/gpio", end='')
+                    print(key, end='')
+                    print("/direction")
+                    
+                    print("echo 1 > /sys/class/gpio/gpio", end='')
+                    print(key, end='')
+                    print("/value")
+                else:
+                    continue
+            except:
+                continue
